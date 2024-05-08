4028c83ba8525f40abe80d55d11e886d20a5e8fb
Bassant-Adel <baadel404@gmail.com>
2024-05-08 12:23:08 -0700

    Markdown to HTML :D

---
+++ markdown2html.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Markdown is awesome! All README.md are made in Markdown
+"""
+import sys
+import os
+import re
+
+
+def convert_markdown_to_html(input_file, output_file):
+    """
+    Markdown is awesome! All README.md are made in Markdown
+    """
+
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(input_file, encoding="utf-8") as f:
+        html_lines = []
+        for line in f:
+
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                heading_level = len(match.group(1))
+                heading_text = match.group(2)
+                html_lines.append
+                (f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                html_lines.append(line.rstrip())
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(html_lines))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>",
+              file=sys.stderr)
+        sys.exit(1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    convert_markdown_to_html(input_file, output_file)
