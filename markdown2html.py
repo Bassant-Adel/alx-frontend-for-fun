d8bf520b7ee1f115ebd1d0f6f538ad7da7053724
Bassant-Adel <baadel404@gmail.com>
2024-05-08 12:29:41 -0700

    Markdown to HTML 2 :D

--- markdown2html.py
+++ markdown2html.py
@@ -6,39 +6,38 @@ import sys
 import os
 import re
 
+if __name__ == '__main__':
 
-def convert_markdown_to_html(input_file, output_file):
-    """
-    Markdown is awesome! All README.md are made in Markdown
-    """
-
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-
-    with open(input_file, encoding="utf-8") as f:
-        html_lines = []
-        for line in f:
-
-            match = re.match(r"^(#+) (.*)$", line)
-            if match:
-                heading_level = len(match.group(1))
-                heading_text = match.group(2)
-                html_lines.append
-                (f"<h{heading_level}>{heading_text}</h{heading_level}>")
-            else:
-                html_lines.append(line.rstrip())
-
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(html_lines))
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file> <output_file>",
+    # Test that the number of arg
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
         sys.exit(1)
+
+    # Store the arg
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    convert_markdown_to_html(input_file, output_file)
+    # Checks that the markdown
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
+
+    with open(input_file, encoding='utf-8') as file_1:
+        html_content = []
+        md_content = [line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r'#{1,6} ', line)
+            if len(heading) > 1:
+                # Compute the number
+                # determine heading
+                h_level = len(line[:line.find(heading[1])-1])
+                # Append the html equivalent
+                html_content.append(
+                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
+                )
+            else:
+                html_content.append(line)
+
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
